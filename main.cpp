cee0d788e778438f688c00108e04e23c23341f3f
Mahdi Taani <mahdi.taani@ed.ac.uk>
2017-12-05 16:25:50 +0000

    Saves changes to a root file.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "TH1D.h"
 #include "TH2D.h"
 #include "TCanvas.h"
-
+#include "TFile.h"
 
 int main() {
 
@@ -15,6 +15,8 @@ int main() {
     int numToGen = 1000; //Number of muons to generate.
 
     std::vector<Muon> muons = GenMuons(numToGen,0.5,1); //Generates muons (number of muons, lowest energy, highest energy) uniform energy distribution
+
+    TFile *f1 = new TFile("SimulationResults.root", "RECREATE");
 
     //TH2D *h1 = new TH2D ("Number of Hits", "Number of Hits", 20, -1, 1, 1-, 0, 1);
     TH1D *h1 = new TH1D ("Number of Hits", "Number of Hits", 100,0, 100);
@@ -32,6 +34,9 @@ int main() {
 
     TCanvas *c1 = new TCanvas();
     h1->Draw();
-    std::cout << "Hello, World!" << std::endl;
+    f1->Write();
+    f1->Close();
+
+
     return 0;
 }
